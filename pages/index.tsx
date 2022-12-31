2cf4fb6a8076de54d5bc34e9c96173eb854439e1
Michi-gi <michi.gm@gmail.com>
2022-12-31 15:56:03 +0900

    change homepage component into function component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,14 @@ import Head from 'next/head'
 import Link from 'next/link'
 import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
+import { NextPage } from 'next'
 import { useState } from 'react'
 
 import { SearchResult } from './ResultType'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export  default function Home() {
+const Home:NextPage = () => {
   const [query, setQuery] = useState("");
   const [result, setResult] = useState({} as SearchResult);
 
@@ -46,3 +47,5 @@ export  default function Home() {
     </>
   );
 }
+
+export default Home;
