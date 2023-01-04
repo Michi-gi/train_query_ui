7c4bd9767ac9114995df5fb62511bae05ebe7f60
Michi-gi <michi.gm@gmail.com>
2023-01-05 00:09:20 +0900

    cache table result

--- pages/station/[stationId]/[tableId]/index.tsx
+++ pages/station/[stationId]/[tableId]/index.tsx
@@ -19,31 +19,40 @@ const Table:NextPage = () => {
 
   const kind = router.query.kind as string || "-1"
   const [selectedKind, setSelectedKind] = useState(kind);
-
+  console.log(`station ID: ${stationId}`)
+  console.log(`table ID: ${tableId}`)
+  console.log(`initial kind: ${kind}`)
+  console.log(`initial selected kind: ${selectedKind}`)
   const statusContext = useContext(StatusContext);
-  let table = statusContext.table[`${tableId}_${kind}`];
 
   useEffect(() => {
-    const query = (selectedKind == "-1") ? "" : `?kind=${selectedKind}`;
-    router.push(`/station/${stationId}/${tableId}${query}`, undefined, { shallow: true})
-    fetch(`/api/station/${stationId}/${tableId}${query}`).then(async (response) => {
-      table = await response.json() as Table;
-      setName(table.lineName);
-      if (table.table) {
-        table.table.forEach((train) => {
-          setHourMinuteInTrain(train);
-        });
-      }
-      setTrains(table.table);
-      setDayMap(table.dayOfWeekMap);
-      statusContext.table[`${tableId}_${kind}`] = table;
-    });
+    const contextKey = `${tableId}_${selectedKind}`;
+    let table = statusContext.table[contextKey];
+    if (table == null) {
+      const query = (selectedKind == "-1") ? "" : `?kind=${selectedKind}`;
+      console.log(`fetched station ID: ${stationId}`)
+      console.log(`fetched table ID: ${tableId}`)
+      console.log(`fetched selected kind: ${selectedKind}`)
+      router.push(`/station/${stationId}/${tableId}${query}`, undefined, { shallow: true})
+      fetch(`/api/station/${stationId}/${tableId}${query}`).then(async (response) => {
+        table = await response.json() as Table;
+        setName(table.lineName);
+        if (table.table) {
+          table.table.forEach((train) => {
+            setHourMinuteInTrain(train);
+          });
+        }
+        setTrains(table.table);
+        setDayMap(table.dayOfWeekMap);
+        statusContext.table[`${tableId}_${kind}`] = table;
+      });
+    }
   }, [selectedKind]);
 
   return (
     <>
       <Head>
-        <title>Station</title>
+        <title>Table</title>
         <meta name="description" content="Station" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
