dfd14a5f40e82842264e9b01f07bddc40bb969ae
Michi-gi <michi.gm@gmail.com>
2023-01-06 01:25:19 +0900

    page transition

--- pages/station/[stationId]/[tableId]/index.tsx
+++ pages/station/[stationId]/[tableId]/index.tsx
@@ -8,45 +8,81 @@ import React, { useState, useEffect, useContext } from 'react'
 import { Table, Train, setHourMinuteInTrain } from 'lib/ResultType'
 import { StatusContext }from "lib/Contexts"
 
-const Table:NextPage = () => {
+const TablePage:NextPage = () => {
   const [name, setName] = useState("");
   const [trains, setTrains] = useState([] as Train[]);
   const [dayMap, setDayMap] = useState({} as {[key: string]: string})
   
   const router = useRouter();
-  const stationId = router.query.stationId;
+  console.log(router)
+  const stationId = router.query.stationId as string;
   const tableId = router.query.tableId as string;
 
   const kind = router.query.kind as string || "-1"
   const [selectedKind, setSelectedKind] = useState(kind);
+  const [viewKind, setViewKind] = useState(kind);
   console.log(`station ID: ${stationId}`)
   console.log(`table ID: ${tableId}`)
   console.log(`initial kind: ${kind}`)
   console.log(`initial selected kind: ${selectedKind}`)
   const statusContext = useContext(StatusContext);
 
-  useEffect(() => {
-    const contextKey = `${tableId}_${selectedKind}`;
+  async function refresh(stationId: string, tableId: string, kind: string): Promise<Table> {
+    const contextKey = `${tableId}_${kind}`;
+    console.log(contextKey)
+    console.log(statusContext.table)
     let table = statusContext.table[contextKey];
-    if (table == null) {
-      const query = (selectedKind == "-1") ? "" : `?kind=${selectedKind}`;
-      console.log(`fetched station ID: ${stationId}`)
-      console.log(`fetched table ID: ${tableId}`)
-      console.log(`fetched selected kind: ${selectedKind}`)
-      router.push(`/station/${stationId}/${tableId}${query}`, undefined, { shallow: true})
-      fetch(`/api/station/${stationId}/${tableId}${query}`).then(async (response) => {
-        table = await response.json() as Table;
-        setName(table.lineName);
+    if (table != null) {
+      console.log("cache hit")
+      return table
+    } else {
+      console.log("cache not hit")
+      const query = (kind == "-1") ? "" : `?kind=${kind}`;
+      return fetch(`/api/station/${stationId}/${tableId}${query}`).then(async (response) => {
+        const table = await response.json() as Table;
+        console.log(table)
         if (table.table) {
           table.table.forEach((train) => {
             setHourMinuteInTrain(train);
           });
         }
-        setTrains(table.table);
-        setDayMap(table.dayOfWeekMap);
-        statusContext.table[`${tableId}_${kind}`] = table;
+        statusContext.table[contextKey] = table;
+        return table;
       });
     }
+  }
+  
+  useEffect(() => {
+    console.log("effected")
+    setViewKind(selectedKind);
+
+    router.beforePopState(({ url, as, options }) => {
+      const urlPattern = /^\/station\/(?<stationId>\w+)\/(?<tableId>\w+)\?kind=(?<kind>\w+)$/;
+      const matchResult = urlPattern.exec(as);
+      if ((matchResult === null) || (matchResult.groups == null)) {
+        router.push(as);
+        return false;
+      }
+      const stationId = matchResult.groups.stationId;
+      const tableId = matchResult.groups.tableId;
+      const kind = matchResult.groups.kind;
+      setViewKind(kind);
+      refresh(stationId, tableId, kind).then((table) => {
+        setName(table.lineName);
+        setTrains(table.table);
+        setDayMap(table.dayOfWeekMap);
+      });
+      return true;
+    });
+
+    refresh(stationId, tableId, selectedKind).then((table) => {
+      console.log("effect refresh")
+      setName(table.lineName);
+      setTrains(table.table);
+      setDayMap(table.dayOfWeekMap);
+      const query = (selectedKind == "-1") ? "" : `?kind=${selectedKind}`;
+      router.push(`/station/${stationId}/${tableId}${query}`, undefined, {shallow: true})
+    });
   }, [selectedKind]);
 
   return (
@@ -62,7 +98,7 @@ const Table:NextPage = () => {
         <div className="btn-group" role="group" aria-label="Basic radio toggle button group">
         {Object.keys(dayMap).map((key) => 
         <React.Fragment key={key}>
-          <input type="radio" className="btn-check" name="btnradio" id={`btnradio_${key}`} value={key} checked={key == selectedKind} onChange={e => setSelectedKind(e.target.value)} />
+          <input type="radio" className="btn-check" name="btnradio" id={`btnradio_${key}`} value={key} checked={key == viewKind} onChange={e => setSelectedKind(e.target.value)} />
           <label className="btn btn-outline-primary" htmlFor={`btnradio_${key}`}>{dayMap[key]}</label>
         </React.Fragment >
         )}
@@ -79,4 +115,4 @@ const Table:NextPage = () => {
   );
 }
 
-export default Table
+export default TablePage
